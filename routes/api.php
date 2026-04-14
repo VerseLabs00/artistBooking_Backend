b89a0e267d05503757c18304cc0452067e885100
Supun Nethsara <supunmax663@gmail.com>
2026-04-14 18:56:09 +0530

    Routes structure changed

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,3 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return 'route working';
-});
+require __DIR__.'/modules/auth.php';
