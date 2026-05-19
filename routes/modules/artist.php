fbfc7e74cac8085f4624248b2f1a655ffd246751
Supun Nethsara <supunmax663@gmail.com>
2026-05-19 16:24:41 +0530

    Fix cover pic updated issue

--- routes/modules/artist.php
+++ routes/modules/artist.php
@@ -9,7 +9,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/profile', [ArtistProfileController::class, 'show']);
     Route::put('/profile', [ArtistProfileController::class, 'update']);
-    Route::post('/profile/media', [ArtistProfileController::class, 'updateMedia']);
+    Route::match(['post', 'put'], '/profile/media', [ArtistProfileController::class, 'updateMedia']);
     Route::post('/profile/gallery', [ArtistProfileController::class, 'addMedia']);
     Route::delete('/profile/gallery/{id}', [ArtistProfileController::class, 'deleteMedia']);
     Route::post('/profile/sync-links', [ArtistProfileController::class, 'syncExternalLinks']);
